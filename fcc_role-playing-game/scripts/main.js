32f97af5b6385c214c8fcb4d5ed4ccdd4c292574
PrakGov <55034062+prak112@users.noreply.github.com>
2024-02-08 19:50:24 +0200

    initial commit

---
+++ fcc_role-playing-game/scripts/main.js
@@ -0,0 +1,12 @@
+// initialize player status
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+
+// initialize characters
+let monsterHealth;
+
+// initialize inventory
+let inventory = ["stick"];
+
