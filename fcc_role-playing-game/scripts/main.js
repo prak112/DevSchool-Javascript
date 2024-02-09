203fce363d993d31bf4c5ca271bb59339df1062f
PrakGov <55034062+prak112@users.noreply.github.com>
2024-02-09 16:52:24 +0200

    front end game intro

--- fcc_role-playing-game/scripts/main.js
+++ fcc_role-playing-game/scripts/main.js
@@ -10,3 +10,8 @@ let monsterHealth;
 // initialize inventory
 let inventory = ["stick"];
 
+// declare buttons
+const button1 = document.getElementById("button1"); // or document.querySelector("#button1")
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+
