f01358830e944ad6838854ea832ef5bc2851b8f6
PrakGov <55034062+prak112@users.noreply.github.com>
2024-03-15 19:40:30 +0200

    use functional components and state for gameboard

--- src/App.js
+++ src/App.js
@@ -1,3 +1,44 @@
-export default function Square() {
-    return <button className="square">X</button>;
-  }  
+import React from "react";
+import { useState } from "react";
+
+// gameboard square
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick(){
+    setValue('X');  /* updates 'value' to X, while 'setValue' remains null */
+  }
+  return (
+    <>
+      <button 
+        className="square"
+        onClick={handleClick}
+      >
+        {value} 
+      </button>
+    </>
+  );
+}
+
+// Gameboard
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}  
