27b0b7f1f23b544e81ffdbb3c1a8aae5755c147f
PrakGov <55034062+prak112@users.noreply.github.com>
2024-04-03 21:07:11 +0300

    setup dev env

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// import all Bootstrap's JS
+import * as Bootstrap from 'bootstrap';
+
+// import specific plugins from Bootstrap
+// import Alert from 'bootstrap/js/dist/alert'
+// import { Tooltip, Toast, Popover } from 'bootstrap';
